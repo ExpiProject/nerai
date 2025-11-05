bdfcd2856222183f9c2061f6ecde090e6f7fb10a
MrYapikZ <yafialhaidar18@gmail.com>
2025-11-05 12:13:47 +0700

    fix: ts config

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,17 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        '@lydell/node-pty',
+        '@lydell/node-pty-darwin-arm64',
+        '@lydell/node-pty-darwin-x64',
+        '@lydell/node-pty-linux-x64',
+        '@lydell/node-pty-win32-x64',
+        '@lydell/node-pty-win32-ia32',
+      ],
+    },
+  },
+});
