e5d6122da5daca75bba7ff6e0b5398f9721f9b9e
MrYapikZ <yafialhaidar18@gmail.com>
2025-11-06 12:58:11 +0700

    feat: setup primeui theme

--- src/renderer.ts
+++ src/renderer.ts
@@ -28,8 +28,22 @@
 import { createApp } from 'vue';
 import './index.css';
 import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            cssLayer: {
+                name: 'primevue',
+                order: 'theme, base, primevue'
+            }
+        }
+    }
+});
+app.mount('#app');
 
 console.log(
   '👋 This message is being logged by "renderer.ts", included via Vite',
